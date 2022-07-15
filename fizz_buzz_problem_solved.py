64f716e1baf709273a32d7e38c7d8681cfaa0913
sonofparker <109389350+sonofparker@users.noreply.github.com>
2022-07-15 12:19:47 -0900

    Add files via upload

---
+++ fizz_buzz_problem_solved.py
@@ -0,0 +1,12 @@
+# solving the popular "fizz_buzz" problem
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "fizz"
+    if input % 5 == 0:
+        return "buzz"
+    return input
+
+
+print(fizz_buzz(7))
